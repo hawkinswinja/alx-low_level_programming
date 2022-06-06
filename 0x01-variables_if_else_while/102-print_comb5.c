b0628e86f3205682e6e0f3c29af21aced0727d3f
hawkinswinja <hawkinswinja@gmail.com>
2022-06-06 11:00:22 -0700

    display two double digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,20 +6,31 @@
  */
 int main(void)
 {
-	int a, b, c,d;
+	int a, b, c, d;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 9; b++)
 		{
-			for (c = 0; c < 10; c++)
+			for (c = a; c < 10; c++)
 			{
 				for (d = b + 1; d < 10; d++)
 				{
-					printf("%d%d %d%d,",a,b,c,d );
+					putchar('0' + a);
+					putchar('0' + b);
+					putchar(32);
+					putchar('0' + c);
+					putchar('0' + d);
+					if (a == 9 && b == 8 && c == d)
+					{
+						break;
+					}
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
