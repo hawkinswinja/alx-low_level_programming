92d71a73b59e406b572a738ddb7e29b720051e5d
hawkinswinja <hawkinswinja@gmail.com>
2022-06-07 14:14:58 -0700

    numbers in order till 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print numbers to 98
+ * @n: starting number
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				printf("\n");
+				break;
+			}
+			printf(", ");
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				printf("\n");
+				break;
+			}
+			printf(", ");
+			n++;
+		}
+	}
+}
