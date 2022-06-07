afa2c53e0b43dccaa9d73d47ba83a81d430f392b
hawkinswinja <hawkinswinja@gmail.com>
2022-06-07 09:50:04 -0700

    determine last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - return last digit of given number
+ * @n: number to determine last digit
+ * Return: lastn
+ */
+int print_last_digit(int n)
+{
+	int lastn;
+
+	lastn = n % 10;
+	return (lastn);
+}
