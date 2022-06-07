e7acd3a991156a7653b0f3cf7213a00976e57fda
hawkinswinja <hawkinswinja@gmail.com>
2022-06-07 12:13:29 -0700

    display time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,8 +9,12 @@ void jack_bauer(void)
 
 	for (h2 = 0; h2 <= 2; h2++)
 	{
-		for (h1 = 0; h1 <= 3; h1++)
+		for (h1 = 0; h1 <= 9; h1++)
 		{
+			if (h2 == 2 && h1 ==4)
+			{
+				break;
+			}
 			for (m2 = 0; m2 <= 5; m2++)
 			{
 				for (m1 = 0; m1 <= 9; m1++)
