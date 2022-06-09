b19aa3fc8504993011d2823c48fc51d1156e7c09
hawkinswinja <hawkinswinja@gmail.com>
2022-06-09 13:25:56 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - print line of given number
+ * @n: number of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = n / -1;
+		}
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
