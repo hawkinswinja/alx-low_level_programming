8a5559f25464fd0e04e455f4a11109e0476bb6aa
hawkinswinja <hawkinswinja@gmail.com>
2022-06-09 15:38:18 -0700

    display triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle using #
+ * @size: base of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int h, b;
+
+		for (h = 1; h <= size; h++)
+		{
+			for (b = 1; b <= (size - h); b++)
+			{
+				_putchar(32);
+			}
+			for (c = 1; c <= h; c++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
