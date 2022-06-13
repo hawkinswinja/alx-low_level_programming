c7d0c97c7e1dfeaa7ad564ec8e397eab84c8efa5
hawkinswinja <hawkinswinja@gmail.com>
2022-06-13 14:47:28 -0700

    characters in 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - puts chars in 2
+ * @str: string of characters
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
