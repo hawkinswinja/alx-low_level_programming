d679977d61b7953f8feca8e41282b927d8f39121
hawkinswinja <hawkinswinja@gmail.com>
2022-06-13 15:27:30 -0700

    print last half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - return last half o string
+ * @str: string to divide
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, n, count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	count = i;
+	if (i % 2 != 0)
+	{
+		i = (i - 1) / 2;
+	}
+	else
+	{
+		i = i / 2;
+	}
+	for (n = i; n < count; n++)
+	{
+		putchar(str[n]);
+	}
+	putchar('\n');
+}
