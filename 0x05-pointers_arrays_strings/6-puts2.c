d1d55ea5f61ae2f365856ebf1a7c90cc83aae6ae
hawkinswinja <hawkinswinja@gmail.com>
2022-06-13 16:08:51 -0700

    characters in 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\\0'; i += 2)
 	{
-		if (str[i] == '\0')
-		{
-			break;
-		}
 		_putchar(str[i]);
 	}
 	_putchar('\n');
