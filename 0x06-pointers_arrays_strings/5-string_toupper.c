570d4f641c5571ebc7872b2d83c6656cc44b378e
hawkinswinja <hawkinswinja@gmail.com>
2022-06-16 09:27:59 -0700

    convert to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -18,22 +18,3 @@ char *string_toupper(char *str)
 	}
 	return (str);
 }
-int main(void)
-
-{
-
-	    char str[] = "Look up!\n";
-
-	        char *ptr;
-
-
-
-		    ptr = string_toupper(str);
-
-		        printf("%s", ptr);
-
-			    printf("%s", str);
-
-			        return (0);
-
-}
