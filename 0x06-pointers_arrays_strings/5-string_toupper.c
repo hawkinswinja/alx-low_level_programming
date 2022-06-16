52a6c1e820dfceb437c1b700c9baa601c39a557d
hawkinswinja <hawkinswinja@gmail.com>
2022-06-16 09:21:58 -0700

    convert to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] > 97 && str[i] < 123)
+		if (str[i] + '0' > 97 && str[i] + '0' < 123)
 		{
 			str[i] = (str[i] - 32);
 		}
