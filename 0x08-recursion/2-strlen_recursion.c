8f7c71571d5d867298e03a618088c0e9101a43fc
hawkinswinja <hawkinswinja@gmail.com>
2022-06-22 15:02:31 +0300

    length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,12 @@
 int _strlen_recursion(char *s)
 {
 	int n = 0, len = 0;
-	++n;
+
 	if (s[n] == '\0')
 	{
-		return (len + 1);
+		return (len);
 	}
+	++n;
 	len = n + _strlen_recursion(&s[n]);
 	return (len);
 }
