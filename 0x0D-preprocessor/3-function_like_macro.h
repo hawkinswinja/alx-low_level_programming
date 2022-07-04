551dd9ed4d2496a5186d93f149c6a615a798ffca
hawkinswinja <hawkinswinja@gmail.com>
2022-07-04 18:35:22 +0300

    more macros definition

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_ABS_
+#define _FUNCTION_ABS_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _ADD_
+#define _ADD_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
