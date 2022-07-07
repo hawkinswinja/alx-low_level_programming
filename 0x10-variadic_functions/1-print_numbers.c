8dd3506f2d6ebc4a3410fa4d12bd1792dceb9363
hawkinswinja <hawkinswinja@gmail.com>
2022-07-07 14:48:46 +0300

    variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,6 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(print);
-	_putchar('\n');
 }
