521693aad38475aa3ee5f1e72dda50d97cfad34f
hawkinswinja <hawkinswinja@gmail.com>
2022-07-25 22:44:12 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,15 +7,17 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wr;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	if (text_content)
-		write(fd, text_content, strlen(text_content));
+		wr = write(fd, text_content, strlen(text_content));
 	close(fd);
+	if (wr < 0)
+		return (-1);
 	return (1);
 }
