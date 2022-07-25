74b53b3d5456f0571613e8406a8e8e78f28a7b44
hawkinswinja <hawkinswinja@gmail.com>
2022-07-25 20:11:16 +0300

    read file and print to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * read_textfile - read contents of file and print to stdout
+  * @filename: path of file name
+  * @letters: number of char to read
+  * Return: number of char read, 0 on failure
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wr;
+	char *c = malloc(sizeof(char) * letters + 1);
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	rd = read(fd, c, letters);
+	if (rd < 0)
+		return (0);
+	wr = write(STDOUT_FILENO, c, strlen(c));
+	if (wr < 0 || wr < rd)
+		return (0);
+	return (wr);
+}
