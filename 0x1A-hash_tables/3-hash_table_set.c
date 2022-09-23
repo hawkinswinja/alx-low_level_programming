db80a4312b5e494be97d868f8559301566dea63e
hawkinswinja <hawkinswinja@gmail.com>
2022-09-23 19:50:27 +0300

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,11 +32,3 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = new;
 	return (1);
 }
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    return (EXIT_SUCCESS);
-}
