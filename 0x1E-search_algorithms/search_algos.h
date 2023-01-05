baa1dadeaf8f52e6c4f6a74ab34e2e9cfc820d01
hawkinswinja <hawkinswinja@gmail.com>
2023-01-05 20:32:09 +0300

    added header file for search algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,3 +1,6 @@
+#ifndef SEARCH
+#define SEARCH
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *list, size_t size);
+#endif
